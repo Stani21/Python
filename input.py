11767303e228eab8af1e473d6f2cb035e2119453
Stani21 <stanish.7t@gmail.com>
2024-09-30 12:19:08 +0530

    feat : format function included

--- input.py
+++ input.py
@@ -2,12 +2,14 @@
 #input() - default function
 #name - variable
 #print-output function
+print("Enter your name")
 name=input("Enter your name: ")
 print(name)
 
 
 #getting input from user as integer of float
 #int-converting to number(Type casting)
+print("Enter a number")
 sur_name=int(input("Enter the number: "))
 print(sur_name)
 
@@ -17,3 +19,34 @@ import getpass
 password =getpass.getpass("Enter a string: ")
 print("password Entered")
 #store u r input in password variable display the print statement
+
+#this function is used to format the string the place where it should need to replace by {}
+print("This is about format function")
+first_name=input("Enter your first name: ")
+last_name =input("Enter your last name: ")
+print("Hello {} and your last name is {} is that correct?".format(first_name,last_name))
+
+
+#1. Print String
+# Statement:
+# =========
+# Write a Python program to print the following string in a specific format (see the output).
+
+# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" 
+
+# Output :
+# Twinkle, twinkle, little star,
+#     How I wonder what you are! 
+#         Up above the world so high,
+#         Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+#     How I wonder what you a
+
+print("""Twinkle, twinkle, little star,
+        How I wonder what you are! 
+        Up above the world so high,
+         Like a diamond in the sky.  
+     Twinkle, twinkle, little star, 
+         How I wonder what you a""")
+
+#DOCUMENTATION___STRING
